82620251c9066ec7a8ba5da6393863d55f8a103b
Kurisu <christofer-heyer@t-online.de>
2017-11-08 22:12:48 +0100

    Added Comments and cleaned code.

--- src/FPKG.c
+++ src/FPKG.c
@@ -12,210 +12,154 @@
 #define MAX_ITERATION 100
 #define NUM_INSTANCES = 100
 
-float randomFloat()
-{
-	// rand() % (max_number + 1 - minimum_number) + minimum_number
 
+// Random Function to fill perceptron weights.
+
+float randomFloat() {
+
+	// rand() % (max_number + 1 - minimum_number) + minimum_number
 	return rand() % (500 + 1 - 5) + 5;
 }
 
-
-
 /*
- * public domain strtok_r() by Charlie Gordon
- *
- *   from comp.lang.c  9/14/2007
- *
- *      http://groups.google.com/group/comp.lang.c/msg/2ab1ecbb86646684
- *
- *     (Declaration that it's public domain):
- *      http://groups.google.com/group/comp.lang.c/msg/7c7b39328fefab9c
+ * So far just put a CSV file titled "test.csv" in the same folder and this Program will read line for line and print in console.
+ * TODO: Implement Perceptron
  */
-
-
-
-int array(int num1, int num2) {
-
-   /* local variable declaration */
-   int result;
-
-   if (num1 > num2)
-      result = num1;
-   else
-      result = num2;
-
-   return result;
-}
-
-
-
 
 int main() {
 
+	// Allocating enough Space for the Char Arrays. Increase if you have lines with more than 6000 Characters
+	char test_data[6000] = "";
+	int value[6000] = { };
+	int readval = 0;
+	int counter = 0;
+	int count = 0;
 
 
-
-
-
-     char test_data[6000] = "";
-	  int value[6000]  ={};
-	  int readval = 0;
-	  int counter = 0;
-	  int count = 0;
-	  char temparr[6000] = "";
-	  double perceptron [784] = {};
-
-	for (int iter = 0; iter < 784; iter++)
-	{
+	//Initialize and fill Peceptron with Random Weights.
+	double perceptron[784] = { };
+	for (int iter = 0; iter < 784; iter++) {
 		perceptron[iter] = randomFloat();
 	}
-
 
 	printf("Start Main. \n");
 
 
-	  FILE *file;
-	  file = fopen("test.csv", "r");
-	  int sizex = 0;
-	  int sizey = 0;
-	  char lb;
-	  lb = getc(file);
+	// Originally this Part would read the CSV file and analyse the amount and length of Lines as Y and X respectively. This Info is not used so far as Dynamic memory allocation
+	// turned out to be rather difficult in C.
+	FILE *file;
+	file = fopen("test.csv", "r");
+	int sizex = 0;
+	int sizey = 0;
+	char lb;
+	lb = getc(file);
 
+	while (lb != '\n') {
 
+		if (lb == ',') {
+			sizex++;
+		}
+		lb = getc(file);
+	}
+	sizey++;
 
- while ( lb !='\n') {
-
-	if(lb ==',') {sizex++;}
-	  lb = getc(file);}
-	  sizey++;
-
-	  while ((lb = getc(file)) != EOF)
-	  {
-		  if( lb == '\n')
-			  sizey++;
-	  }
-
+	while ((lb = getc(file)) != EOF) {
+		if (lb == '\n')
+			sizey++;
+	}
 
 	// printf("%d", sizex);
-	 // printf("\n");
-	 // printf("%d", sizey);
-	 // printf("\n");
+	// printf("\n");
+	// printf("%d", sizey);
+	// printf("\n");
 
-	  fclose(file);
+	fclose(file);
 
 
+	//--------------------------------------//
 
+	// -- This is the important part! -- //
+	// Here we start by opening the File and reading the it line by line.
+	file = fopen("test.csv", "r");
+	printf("\n");
+	printf("Start reading into array.\n");
 
+	if (file) {
 
-  file = fopen("test.csv", "r");
+		// It reads the File Char by Char until the End of File.
+		while ((readval = getc(file)) != EOF) {
 
+			// Once a line is completed, we have a finished array and extract the int Values.
+			if (readval == '\n') {
 
 
+				char *str = test_data;
+				char *end = str;
+				count = 0;
 
+				// Here we extract int values via strtol and return a clean int array, rather than a char array with breaks.
+				while (*end) {
+					int n = strtol(str, &end, 10);
 
+					value[count] = n;
+					count++;
+					//printf("%d\n", n);
+					while (*end == ',') {
+						end++;
+					}
+					str = end;
 
+				}
 
-  printf("\n");
-  printf("Start reading into array.\n");
 
-  if (file) {
-      while ((readval = getc(file)) != EOF){
+//--------------------------------------------------------------//
 
+//TODO: Here let the Perceptron work with the 785 int array of values.
 
+				printf("Learn.\n");
 
-    	  if (readval == '\n'){
+				for (int i = 1; i < sizeof(perceptron); i++) {
 
-/*
-        	   for (int i = 0; i < sizeof(test_data); i++) {
-        	      temparr[i] = test_data[i];
-        	   }
+					//	perceptron[i] += LEARNING_RATE * localError * [p];
 
-*/
+				}
+//--------------------------------------------------------------//
 
 
+				// We print the first 100 int values of the line we just read to verify.
+				printf("Start printing array.\n");
 
-//s
-        		char *str = test_data;
-        		char *end = str;
+				for (int i = 0; i < 100; i++) {
 
-        		count = 0;
-        		while(*end) {
-        			int n = strtol(str, &end, 10);
+					printf("%i", value[i]);
+					printf("-");
+				}
 
-        			value[count] = n;
-        			count++;
-        			//printf("%d\n", n);
-        			while (*end == ',') {
-        				end++;
-        			}
-        			str = end;
+				printf("Reset.\n");
+				printf("\n");
 
+				// Reset the Arrays so that we can fill them again. This method uses as little as possible Memory.
+				memset(test_data, 0, sizeof(test_data));
+				memset(value, 0, sizeof(value));
+				counter = 0;
+			}
 
-        		}
 
+			// This is the part where the array is filled while we still haven't encountered a line break. Which is then used above.
+			// We copy character by character from the source file into a Char Array. That is the Array which gets turned into an int array by strtol.
+			test_data[counter] = readval;
+			counter++;
 
+		}
 
 
+		// Finished, maybe think about clearing up Memory to avoid Memory leak?
+		printf("\n");
+		fclose(file);
 
+		printf("Finished, now terminating.\n");
 
-
-        		printf("Learn.\n");
-
-
-
-
-        		for(int i = 1; i < sizeof(perceptron); i++)
-        		{
-        		//	perceptron[i] += LEARNING_RATE * localError * [p];
-
-        		}
-
-
-
-
-
-
-
-
-
-       	 	 printf("Start printing array.\n");
-
-       	       for ( int i = 0; i < 100 ; i++ ) {
-
-       	     	  printf("%i", value[i]);
-       	     	  printf("-");
-       	       }
-
-       	    printf("Reset.\n");
-       	    printf("\n");
-
-           	   memset(test_data, 0, sizeof(test_data));
-           	   memset(value, 0, sizeof(value));
-       	       counter = 0;
-   		  }
-
-
-    	  test_data[counter] = readval;
-    	  counter++;
-
-    	  }
-
-
-
-
-
-
-
-
-
-      printf("\n");
-	  fclose(file);
-
-	  printf("Finished, now terminating.\n");
-
-      return 0;
+		return 0;
+	}
 }
-}
-
-
 
