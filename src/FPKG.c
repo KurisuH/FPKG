d00c0c8d726359b0554501bd877f8012265a25ec
Kurisu <christofer-heyer@t-online.de>
2017-11-09 16:01:52 +0100

    Working Perceptron. Still need to clean the code a bit.

--- src/FPKG.c
+++ src/FPKG.c
@@ -15,10 +15,12 @@
 
 // Random Function to fill perceptron weights.
 
+
 float randomFloat() {
 
 	// rand() % (max_number + 1 - minimum_number) + minimum_number
-	return rand() % (500 + 1 - 5) + 5;
+	return rand() % (5000 + 1 - 0) + 0;
+
 }
 
 
@@ -51,11 +53,35 @@ int calculate_output(float *perceptron, int *data, int size )
 
 }
 
+void extract_values(char *data, int *values, int size)
+{
+
+	char *str = data;
+	char *end = str;
+	int count = 0;
+
+	// Here we extract int values via strtol and return a clean int array, rather than a char array with breaks.
+	while (*end) {
+		int n = strtol(str, &end, 10);
+
+		values[count] = n;
+		count++;
+		//printf("%d\n", n);
+		while (*end == ',') {
+			end++;
+		}
+		str = end;
+
+	}
+}
+
 int main() {
 
 	// Allocating enough Space for the Char Arrays. Increase if you have lines with more than 6000 Characters
 	char test_data[6000] = "";
+	char test_data2[6000] = "";
 	int value[6000] = { };
+	int value2[6000] = { };
 	int readval = 0;
 	int counter = 0;
 	int count = 0;
@@ -92,12 +118,15 @@ int main() {
 			// Once a line is completed, we have a finished array and extract the int Values.
 			if (readval == '\n') {
 
+				/*
 
 				char *str = test_data;
 				char *end = str;
 				count = 0;
 
 				// Here we extract int values via strtol and return a clean int array, rather than a char array with breaks.
+
+
 				while (*end) {
 					int n = strtol(str, &end, 10);
 
@@ -110,6 +139,9 @@ int main() {
 					str = end;
 
 				}
+				*/
+
+				extract_values(test_data,value,785);
 
 
 //--------------------------------------------------------------//
@@ -173,15 +205,110 @@ int main() {
 			counter++;
 
 		}
+	}
 
 
 		// Finished, maybe think about clearing up Memory to avoid Memory leak?
 		printf("\n");
 		fclose(file);
 
+
+		file = fopen("mnist_train_small.csv", "r");
+		printf("\n");
+		printf("Start reading into array.\n");
+
+		int correct = 0;
+		int wrong = 0;
+		int total = 0;
+
+		if (file) {
+
+			while ((readval = getc(file)) != EOF) {
+
+
+
+
+						// Once a line is completed, we have a finished array and extract the int Values.
+						if (readval == '\n') {
+
+
+
+
+
+
+							for (int i = 0; i < 20; i++) {
+							printf("%c", test_data2[i]);
+
+							}
+
+
+							extract_values(test_data2,value2,785);
+
+							if (value2[0] == 1 || value2[0] == 0 ){
+
+								output = calculate_output(perceptron,value2,sizeof(perceptron));
+								int error = evaluate(output, value2[0]);
+
+								if (error == 1){ correct++;} else { wrong++;}
+								total++;
+
+
+
+/*
+							printf("\n");
+
+
+							for (int i = 0; i < 10; i++) {
+
+
+								printf("%i", value2[i]);
+								printf(",");
+
+							}
+							printf("\n");
+	*/
+
+							}
+
+							memset(test_data2, 0, sizeof(test_data2));
+							memset(value2, 0, sizeof(value2));
+							counter = 0;
+
+							// We print the first 100 int values of the line we just read to verify.
+
+
+						}
+
+						// This is the part where the array is filled while we still haven't encountered a line break. Which is then used above.
+						// We copy character by character from the source file into a Char Array. That is the Array which gets turned into an int array by strtol.
+						test_data2[counter] = readval;
+
+						//printf("%c",readval);
+						printf("%c",test_data2[counter]);
+						counter++;
+						}
+
+
+
+
+					}
+
+
+		printf("\n");
+		printf("Total: ");
+		printf("%d", total);
+		printf("\n");
+		printf("Correct ");
+		printf("%d", correct);
+		printf("\n");
+		printf("Wrong ");
+		printf("%d", wrong);
+		printf("\n");
+
 		printf("Finished, now terminating.\n");
 
 		return 0;
 	}
-}
+
+
 
